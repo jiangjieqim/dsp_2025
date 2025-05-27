4c34a95861fa0d28ef71bfec2e1aa856b1e66466
jiangjieqim <jiangjieqim@hotmail.com>
2025-05-28 01:04:40 +0800

    .

--- c51/test1.c
+++ c51/test1.c
@@ -1,5 +1,6 @@
 #include <reg51.h>
 sbit LED = P1^0;
+#define __DELAY__ 100
 static void 
 delay(unsigned int time) {
     unsigned int i, j;
@@ -9,8 +10,8 @@ delay(unsigned int time) {
 void main() {
     while(1) {
         LED = 0;    
-        delay(500); 
+        delay(__DELAY__); 
         LED = 1;   
-        delay(500);
+        delay(__DELAY__);
     }
 }
