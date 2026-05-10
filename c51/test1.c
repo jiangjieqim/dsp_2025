428a0cdadbfee158c5e4c0e9e6bceef5ffa48171
jiangjieqim <jiangjieqim@hotmail.com>
2026-05-11 00:23:45 +0800

    .

--- c51/test1.c
+++ c51/test1.c
@@ -1,22 +1,48 @@
 /*
 2400-115200
 */
-
-
 #include <reg51.h>
-sbit LED = P0^0;
-#define __DELAY__ 1000
-static void 
-delay(unsigned int time) {
+#include <intrins.h>
+
+sbit LED = P0 ^ 0;
+
+#define __DELAY__ 1
+
+static void
+delay(unsigned int time)
+{
+    /*
     unsigned int i, j;
     for(i = 0; i < time; i++)
         for(j = 0; j < 125; j++);
+
+    */
+   /*
+    Simulation is not running in real time due to excessive CPU load.
+    0.012789069s
+   */
+    _nop_();
 }
-void main() {
-    while(1) {
-        LED = 0;    
-        delay(__DELAY__); 
-        LED = 1;   
-        delay(__DELAY__);
+
+// 使用参数time控制延时长度
+static void delayMs(unsigned int time)
+{
+    unsigned int i, j;
+    for(i = 0; i < time; i++)
+        for(j = 0; j < 120; j++)  // 内层循环大约1ms（@12MHz晶振）
+            _nop_();
+}
+
+
+void main()
+{
+    while (1)
+    {
+        LED = 0;
+        _nop_();
+        // delay(__DELAY__);
+        LED = 1;
+        _nop_();
+        // delay(__DELAY__);
     }
 }
