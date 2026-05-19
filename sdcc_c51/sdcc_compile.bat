e2e4c2beb6c0a3239fb1c21486721a2fb07aa2a7
jiangjieqim <jiangjieqim@hotmail.com>
2026-05-20 00:19:03 +0800

    .

--- sdcc_c51/sdcc_compile.bat
+++ sdcc_c51/sdcc_compile.bat
@@ -1,8 +1,14 @@
-set workspaceFolder=%cd%
+@REM set workspaceFolder=%cd%
+@REM sdcc --version
+
+set workspaceFolder=%1
+
+set TARGET=%2
+
+set "basename=%TARGET:.c=%"
+set TARGET=%basename%
 
 set PATH=C:\Program Files\SDCC\bin
-
-sdcc --version
 
 
 if not exist output (
@@ -13,11 +19,10 @@ if not exist output (
 
 del /Q %workspaceFolder%\output\*.*
 
-sdcc -mmcs51 -I. --out-fmt-ihx -o output/ test.c
+sdcc -mmcs51 -I%workspaceFolder%\include --out-fmt-ihx -o output/ %TARGET%.c
 @REM sdcc -mmcs51 -I. --opt-code-size --out-fmt-ihx -o output/ test.c
 cd output
 
+packihx %TARGET%.ihx > %TARGET%.hex
 
-packihx test.ihx > test.hex
-
-pause
+@REM pause
--- sdcc_c51/test.c
+++ sdcc_c51/test.c
@@ -1,10 +1,12 @@
 #include "STC89C5xRC.h"
+
 #define _nop_() __asm nop __endasm
-void main() {
+
+void main(void) {
     while(1) {
-        P1 = 0x0;
+        P0 = 0x0;
         _nop_();
-        P1 = 0x1;
+        P0 = 0x1;
         _nop_();
     }
 }
