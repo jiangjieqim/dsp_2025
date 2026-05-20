da3ef89a6dc78fdf2d3426e56f11a3491f41c466
jiangjieqim <jiangjieqim@hotmail.com>
2026-05-20 13:16:09 +0800

    .

---
+++ sdcc_c51/debug.bat
@@ -0,0 +1,7 @@
+set PATH=C:\Program Files\SDCC\bin
+
+cd output
+
+s51 -X 11059200 -V program.ihx
+
+pause
---
+++ sdcc_c51/test.bat
@@ -0,0 +1,37 @@
+echo off
+
+@REM set workspaceFolder=%cd%
+@REM sdcc --version
+
+set workspaceFolder=%cd%
+
+set TARGET=test
+
+set PATH=C:\Program Files\SDCC\bin
+
+set OUT_DIR=output
+
+if not exist output (
+    mkdir output 2>nul
+) else (
+    del /Q %workspaceFolder%\output\*.*
+)
+
+set program=program
+
+@REM sdcc -mmcs51 -I%workspaceFolder%\include --out-fmt-ihx -o output/ %TARGET%.c
+
+sdcc -c -mmcs51 -Iinclude %TARGET%.c -o %OUT_DIR%\%TARGET%.rel
+sdcc -c -mmcs51 -Iinclude src\delay.c -o %OUT_DIR%\delay.rel
+
+sdcc -mmcs51 %OUT_DIR%\%TARGET%.rel %OUT_DIR%\delay.rel -o %OUT_DIR%\%program%.ihx
+
+
+@REM sdcc -mmcs51 -I%workspaceFolder%\include --opt-code-size --out-fmt-ihx -o output/ %TARGET%.c
+cd output
+
+packihx %program%.ihx > %program%.hex
+for %%A in (%program%.hex) do (
+    echo %program%.hex size %%~zA bytes
+)
+
